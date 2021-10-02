306673530bb978fca72c05d38db98e3dbedc0e2f
akjha271314 <akjha271314@gmail.com>
2021-10-02 18:20:57 +0530

    DELETE added

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,15 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Aashutosh",
-        lastName: "Kashyap",
-        age: 20
-    }
-]
+let users = [];
 
 //all routes in here are starting with /users
 router.get('/', (req, res) => {
 
-
     res.send(users);
-
 
 });
 
@@ -28,9 +17,25 @@ router.post('/', (req, res) => {
 
     const user = req.body;
 
-    users.push(user);
+    users.push({ ... user, id: uuidv4() });
 
     res.send(`User with the name ${user.firstName} added to the database`);
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} deleted from the database.`);
 });
 
 export default router;
